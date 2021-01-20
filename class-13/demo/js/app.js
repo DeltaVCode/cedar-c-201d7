b59ec23020eaf963028effefff38ea3d408819cb
tektechnologies <info@tektechnologies.com>
2021-01-20 15:20:33 -0600

    add working demo

--- class-13/demo/js/app.js
+++ class-13/demo/js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 console.log('app js is connected!');
-//These are in an html collection(array) use ref to image index instead of id's for tracking images shown we are still going to count total clicks and each image's click amount.
 
 var imageElements = document.getElementsByTagName('img');
-console.log('image Elements source',imageElements);
+var nameForm = document.getElementById('nameForm');
 
 var pizzaIndex1 = 0;
 var pizzaIndex2 = 1;
@@ -14,8 +13,6 @@ var allPizzas = [];
 function Pizza(name, imageUrl, timesClicked){
   this.name = name;
   this.imageUrl = imageUrl;
-  //Add in some persistence 
-  // console.log('do we have clicks ',timesClicked);
   if(timesClicked){
     this.timesClicked = timesClicked;
   } else {
@@ -23,72 +20,49 @@ function Pizza(name, imageUrl, timesClicked){
   }
   this.timesShown = 0;
   allPizzas.push(this);
-  // console.log('this is our pizza objects being created: ', this);
 }
 
-
-//-----------Add a function for our chart to render pizza data from our objects
 function getPizzaArray(nameOfThePropertyIWant){
   var answer = [];
-
   for(var i = 0; i < allPizzas.length; i++){
     answer[i] = allPizzas[i][nameOfThePropertyIWant];
   }
-  console.log('name of the property i want ',answer);
   return answer;
 }
 
-
-
-//-------------------- Add this as well -----------------------------------------
-// if there is local storage, go see if its true and get that local storage string and parse it.
 var savedPizzaString = localStorage.getItem('savedPizza');
 if(savedPizzaString){
-  //change from a string to objects that- dont yet know they are pizzas 
   var arrayOfNotYetPizzaObject = JSON.parse(savedPizzaString);
-
- for(var j = 0; j < arrayOfNotYetPizzaObject.length; j++){
-    new Pizza(arrayOfNotYetPizzaObject[j].name, arrayOfNotYetPizzaObject.imageUrl,
-     arrayOfNotYetPizzaObject[j].timesClicked);
- }
-
-
+  for(var j = 0; j < arrayOfNotYetPizzaObject.length; j++){
+    new Pizza(arrayOfNotYetPizzaObject[j].name, arrayOfNotYetPizzaObject[j].imageUrl,
+      arrayOfNotYetPizzaObject[j].timesClicked);
+  }
 } else {
+  new Pizza('New York Thin', 'images/newYorkPizza.jpeg');
+  new Pizza('Detroit Style', 'images/detroitPizza.jpeg');
+  new Pizza('Calzone', 'images/calzonePizza.jpeg');
+  new Pizza('Brick Oven Pizza', 'images/brickOvenPizza.jpeg');
+  new Pizza('Chicago Deep Dish', 'images/chicagoPizza.jpeg');
+  new Pizza('Chicago Pizza and Oven Grinder', 'images/cpoGinderPizza.jpeg');
+  new Pizza('Papa Vito\'s Thin', 'images/mwDeluxePizzaThinCrust.jpg');
+  new Pizza('Shot Gun Dan\'s', 'images/sgDansHtossedMeatLovPizza.jpg');
 
-// actually create our Pizza's
-new Pizza('New York Thin', 'images/newYorkPizza.jpeg');
-new Pizza('Detroit Style', 'images/detroitPizza.jpeg');
-new Pizza('Calzone', 'images/calzonePizza.jpeg');
-new Pizza('Brick Oven Pizza', 'images/brickOvenPizza.jpeg');
-new Pizza('Chicago Deep Dish', 'images/chicagoPizza.jpeg');
-new Pizza('Chicago Pizza and Oven Grinder', 'images/cpoGinderPizza.jpeg');
-new Pizza('Papa Vito\'s Thin', 'images/mwDeluxePizzaThinCrust.jpg');
-new Pizza('Detroit Style', 'images/sgDansHtossedMeatLovPizza.jpg');
-
-}//closes our if else condition so that our object being parsed from a string value can then be added back into our pizza objects by way of our constructor function. 
-
-
-
-//Lets count the first images that show up.
-for(var i = 0; i < 2; i++){
-  allPizzas[i].timesShown++;
-  console.log(allPizzas[i]);
 }
-// allPizzas[0].timesShown = 1;
-// allPizzas[1].timesShown = 1;
+
+allPizzas[0].timesShown = 1;
+allPizzas[1].timesShown = 1;
+
 var totalClicks = 0;
 
 function imageWasClicked(event){
   totalClicks++;
-  // console.log(totalClicks);
   if(event.srcElement.id === '1'){
     allPizzas[pizzaIndex1].timesClicked++;
   } else if (event.srcElement[pizzaIndex2] === '2'){
     allPizzas[pizzaIndex2].timesClicked++;
   }
-  //logic so that we dont see the same images from click to click
   var nextPizzaIndex1 = Math.floor(Math.random() * allPizzas.length);
-  // console.log('next pizza one from our random(): ', nextPizzaIndex1);
+
   while((nextPizzaIndex1 === pizzaIndex1) || (nextPizzaIndex1 === nextPizzaIndex2)){
     nextPizzaIndex1 = Math.floor(Math.random() * allPizzas.length);
   }
@@ -96,23 +70,18 @@ function imageWasClicked(event){
   while((nextPizzaIndex2 === pizzaIndex2) || (nextPizzaIndex2 === nextPizzaIndex1)){
     nextPizzaIndex2 = Math.floor(Math.random() * allPizzas.length);
   }
-  //relfect the updates to the new values next pizzas
-  //set up reference to new pizza images.
+
   pizzaIndex1 = nextPizzaIndex1;
   pizzaIndex2 = nextPizzaIndex2;
 
   imageElements[0].src = allPizzas[pizzaIndex1].imageUrl;
   allPizzas[pizzaIndex1].timesShown++;
 
-  // console.log(allPizzas[pizzaIndex1].timesShown);
   imageElements[1].src = allPizzas[pizzaIndex2].imageUrl;
   allPizzas[pizzaIndex2].timesShown++;
 
   if(totalClicks >= rounds){
-
-    //this grabs our constructor function objects and makes them a string to be stored in localstorage 
     localStorage.setItem('savedPizza', JSON.stringify(allPizzas));
-    //  console.log('saved pizza from our set ITEM: ',  savedPizza);
 
     var asideUl = document.getElementById('voteResults');
     for(var i = 0; i < allPizzas.length; i++){
@@ -129,7 +98,7 @@ function imageWasClicked(event){
       percentageListItem.textContent = `${allPizzas[i].name} percentage of clicked on vs times shown is  ` + math;
 
       asideUl.appendChild(percentageListItem);
-    }// closes 
+    }// closes
 
     for(var x = 0; x < imageElements.length; x++){
       imageElements[x].removeEventListener('click', imageWasClicked);
@@ -143,7 +112,7 @@ function imageWasClicked(event){
 
 function runMyChartsNow(){
   var ctx = document.getElementById('myResultsChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  new Chart(ctx, {
     type: 'doughnut',
     data: {
       labels: getPizzaArray('name'),
@@ -198,23 +167,11 @@ for(var i = 0; i < imageElements.length; i++){
 }
 
 
-//form 
-var nameForm = document.getElementById('nameForm');
-
+//form
 nameForm.addEventListener('submit', function(event){
   event.preventDefault();
-  console.log('name form is listening');
-  //grab what the user typed in
   var nameUserProvided = document.getElementById('name').value;
-  console.log('nameUserProvided: ' ,nameUserProvided);
-  //save to local storage
   localStorage.setItem('userName', nameUserProvided);
-  //show that input on the page itself
-  //get rid of the form
   nameForm.textContent = 'Welcome to our site ' + nameUserProvided;
 });
 
-var savedName = localStorage.getItem('userName');
-if(savedName){
-  nameForm.textContent = `Thanks for stopping by ${savedName} what is your favorite pizza?`;
-}
